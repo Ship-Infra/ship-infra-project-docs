bc3fd865cb569a73dac11cd1b96679dd6f804a95
Viktor Vasylkovskyi <vvasylkovskyi@pagerduty.com>
2025-12-16 16:44:21 +0000

    Add Nameservers

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -7,3 +7,24 @@ provider "aws" {
 resource "aws_route53_zone" "main" {
   name = "ship-infra.com"
 }
+
+resource "aws_route53domains_registered_domain" "main" {
+  domain_name = "ship-infra.com"
+
+  # Current Name Servers
+  dynamic "name_server" {
+    for_each = aws_route53_zone.main.name_servers
+    content {
+      name = name_server.value
+    }
+  }
+
+  # Vercel Nameservers
+  name_server {
+    name = "ns1.vercel-dns.com"
+  }
+
+  name_server {
+    name = "ns2.vercel-dns.com"
+  }
+}
